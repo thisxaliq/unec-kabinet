455f68b87db2b0d4340f248e10b5935a07323183
So... <yerdelenkus@gmail.com>
2026-09-11 19:42:07 +0400

    java update

--- android/app/src/main/java/com/unec/kabinet/MainActivity.java
+++ android/app/src/main/java/com/unec/kabinet/MainActivity.java
@@ -1,5 +1,36 @@
 package com.unec.kabinet;
 
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getBridge().getWebView().setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(
+                    WebView view,
+                    WebResourceRequest request
+            ) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(
+                    WebView view,
+                    String url
+            ) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
